3e416faf86998885557170bf7939e5a6b8a335ab
Kate <chelsea-92@mail.ru>
2016-03-26 21:07:28 +0300

    Адаптивная сетка для index. Исправления. Скрипт для мобильного меню

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Мобильное меню
+// Получаем элементы
+var nav = document.querySelector('.main-nav__list');
+var buttClose = document.querySelector('.main-nav__toggle');
+
+// Класс, обозначающий закрытое меню
+var statusClose = 'main-nav__list--closed';
+
+// Событие по нажатию на кнопку
+buttClose.onclick = function() {
+  nav.classList.toggle( statusClose );
+}
